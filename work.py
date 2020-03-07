197276b3cd951231b41f4a4d7c395d08e73311c9
galvinpet <galvinpetrez@gmail.com>
2020-03-07 03:57:10 +0300

    my work

---
+++ work.py
@@ -0,0 +1,18 @@
+a = '+'
+b = '- '
+k = ' '
+c = '|'
+n = str(a + k + b * 4 + a)
+x = str(k + b * 4 + a)
+v = str(c + k * 9 + c + k * 9 + c)
+print(n + x)
+for i in range(4):
+    print()
+    print(v)
+    print()
+print(n + x)
+for i in range(4):
+    print()
+    print(v)
+    print()
+print(n + x)
